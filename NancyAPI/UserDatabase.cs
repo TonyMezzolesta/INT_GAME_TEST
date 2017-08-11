b8976bb3c9f5ab478b35dd1908f616f46e45c896
amezz <amezz@TONY-LAPTOP-ACE>
2017-08-10 22:41:20 -0400

    removed all token auth

---
+++ NancyAPI/UserDatabase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nancy.Security;
+
+namespace NancyAPI
+{
+    public class UserDatabase
+    {
+        public static IUserIdentity ValidateUser(string p_userName, string p_password)
+        {
+            IUserIdentity returnValue = null;
+
+            //returnValue = "";
+
+            return returnValue;
+        }
+    }
+}
